c365c1d8bae04f289857dd602760e5100d1acb80
theresaliao <theresaliao@itri.org.tw>
2019-04-02 17:02:33 +0800

    production setting

--- auto_build.sh
+++ auto_build.sh
@@ -5,5 +5,5 @@ docker run --name task5-4 \
            -ti -d \
            --network=datasetbridge \
            -p 50010:22 -p 50011:80 \
-           -v /tmp/dataset_doc:/tmp \
+           -v /home/ccma/dataset_doc:/tmp \
            golang_1.11.2:1.0.0
